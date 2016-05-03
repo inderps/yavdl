6d7d4a91e8cb614ef165a56086cd3a4b48984caa
Inderpal Singh <singhinther@gmail.com>
2016-05-03 19:06:42 +0200

    test util file

---
+++ test/testHelper.js
@@ -0,0 +1,9 @@
+import jq from 'jquery';
+import jsdom from 'jsdom';
+import { expect } from 'chai';
+
+global.document = jsdom.jsdom('<!doctype html><html><body><div id="root"></div></body></html>');
+global.window = global.document.defaultView;
+const $ = jq(window);
+
+export { expect, $ };
